41b005cfc71d55f34821c5367917cd37f28bb7e2
kobanyan <kobanyan@users.noreply.github.com>
2019-06-25 14:12:11 +0900

    Fix lint error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 export const even = (num: number): boolean => num % 2 === 0;
-export const odd = (num: number): boolean => {
-  console.log('lint error');
-  return num % 2 === 1;
-}
+export const odd = (num: number): boolean => num % 2 === 1;
